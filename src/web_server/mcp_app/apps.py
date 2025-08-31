bdec3077260a019c4620511fd268131a06becc11
Lukas Jeckle <gitlab@lgndluke.dev>
2025-08-31 21:15:03 +0200

    fix(src/web_server/mcp_app): Corrected mcp_app components name.

--- src/web_server/mcp_app/apps.py
+++ src/web_server/mcp_app/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class McpAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mcp_app'
+    name = 'src.web_server.mcp_app'
