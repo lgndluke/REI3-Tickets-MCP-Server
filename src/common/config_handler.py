e3ec3abec2d6b831f9a2a3f3aa8fd87798e0a6cd
Lukas Jeckle <gitlab@lgndluke.dev>
2025-09-01 10:41:50 +0200

    feat(src/common): Made config_handler use ConfigUpdater.
    This change was made to preserve comments inside the .ini file on change sync via the web administration tool.

--- src/common/config_handler.py
+++ src/common/config_handler.py
@@ -1,12 +1,11 @@
-import configparser
-
+from configupdater import ConfigUpdater
 from pathlib import Path
 
 # ----------------------------
 # Load configuration file
 # ----------------------------
 
-config = configparser.ConfigParser()
+config = ConfigUpdater()
 path   = Path(__file__).resolve().parents[2] / "config.ini"
 config.read(path)
 
@@ -18,7 +17,7 @@ def get_config_value(section: str, key: str, fallback='') -> str:
     """
     Returns a configuration value from the given section and key.
     """
-    return config.get(section, key, fallback=fallback)
+    return config.get(section, key, fallback=fallback).value
 
 def set_config_value(section: str, key: str, value: str) -> None:
     """
