cc64ea813139dc7df1e7f6718bbc9300458c6be2
Lukas Jeckle <gitlab@lgndluke.dev>
2025-09-01 15:14:27 +0200

    feat(src/web_server/main): asgi.py exposes MCP server and admin ui.

--- src/web_server/main/asgi.py
+++ src/web_server/main/asgi.py
@@ -10,7 +10,23 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from fastapi import FastAPI
+from fastmcp import settings
+from src.rei3.tickets.mcp.server import REI3TicketsMCPServer
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.web_server.main.settings')
 
-application = get_asgi_application()
+# Set FastMCP settings to expose server endpoint at '/'. Default would be '/mcp'.
+settings.streamable_http_path = '/'
+
+# Get ASGI applications.
+django_asgi_application = get_asgi_application()
+tickets_mcp_endpoint    = REI3TicketsMCPServer().get_fastmcp().http_app()
+
+# Mount ASGI applications to root FastAPI application.
+root = FastAPI(lifespan=tickets_mcp_endpoint.lifespan)
+root.mount('/mcp', tickets_mcp_endpoint)
+root.mount('/', django_asgi_application)
+
+# Set application to root FastAPI application.
+application = root
