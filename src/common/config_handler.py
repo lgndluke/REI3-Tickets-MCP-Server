3cc8c186dbda67566a0ddecad0d2ad0095bf1581
Lukas Jeckle <gitlab@lgndluke.dev>
2025-09-01 10:29:07 +0200

    feat(src/common): Added save method and improved config_handler comments

--- src/common/config_handler.py
+++ src/common/config_handler.py
@@ -15,7 +15,21 @@ config.read(path)
 # ----------------------------
 
 def get_config_value(section: str, key: str, fallback='') -> str:
+    """
+    Returns a configuration value from the given section and key.
+    """
     return config.get(section, key, fallback=fallback)
 
 def set_config_value(section: str, key: str, value: str) -> None:
+    """
+    Set a configuration value in the given section.
+    (The changes are only applied in memory, not persisted on disk! To persist call save_config())
+    """
     config.set(section, key, value)
+
+def save_config() -> None:
+    """
+    Write the current config state to the config.ini file.
+    """
+    with open(path, 'w') as config_file:
+        config.write(config_file)
