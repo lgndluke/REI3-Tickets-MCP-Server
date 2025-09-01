94eea38458b1b00e74d6a03a2c11d22111b062af
Lukas Jeckle <gitlab@lgndluke.dev>
2025-09-01 16:09:22 +0200

    fix(src/web_server/main): Fixed ASGI static file handling issues.

--- src/web_server/main/asgi.py
+++ src/web_server/main/asgi.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from django.contrib.staticfiles.handlers import ASGIStaticFilesHandler
 from fastapi import FastAPI
 from fastmcp import settings
 from src.rei3.tickets.mcp.server import REI3TicketsMCPServer
@@ -20,7 +21,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.web_server.main.settings')
 settings.streamable_http_path = '/'
 
 # Get ASGI applications.
-django_asgi_application = get_asgi_application()
+django_asgi_application = ASGIStaticFilesHandler(get_asgi_application())
 tickets_mcp_endpoint    = REI3TicketsMCPServer().get_fastmcp().http_app()
 
 # Mount ASGI applications to root FastAPI application.
